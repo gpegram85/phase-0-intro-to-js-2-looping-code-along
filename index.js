af8fc625c69fcec67023e2b67cabb980b757ff2a
Grey Pegram <dpegram85@gmail.com>
2023-12-13 21:53:43 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = [];
+
+function writeCards(names, event) {
+    const newNames = [];
+
+    for (let i = 0; i < names.length; i++) {
+        //code to add msg to new array
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newNames;
+}
+
+writeCards(newNames);
+
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int--);
+    }
+}
+
